dbd3e8c4422a82808ae99084c68b6f415e03639f
tasklet1579 <tasklet1579@gmail.com>
2022-05-21 10:53:03 +0900

    연관관계 매핑

--- src/main/java/jpa/Main.java
+++ src/main/java/jpa/Main.java
@@ -1,5 +1,7 @@
 package jpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -7,6 +9,7 @@ import javax.persistence.Persistence;
 
 import jpa.entity.Member;
 import jpa.entity.MemberType;
+import jpa.entity.Team;
 
 public class Main {
 
@@ -18,23 +21,110 @@ public class Main {
 		// JPA의 모든 데이터 변경은 트랜잭션 안에서 실행한다
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 		EntityManager em = emf.createEntityManager();
+		
+//		fieldToColumnMapping(em);
+		
+//		oneWayAssociationRelationshipMapping(em);
+		
+		bothWayAssociationRelationshipMapping(em);
+		
+		emf.close();
+	}
+	
+	public static void fieldToColumnMapping(EntityManager em) {
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		try {			
+			tx.begin();
+			
+			Member member = new Member();		
+//			member.setId(100L);
+			member.setName("안녕하세요");
+//			member.setMemberType(MemberType.USER);
+			em.persist(member);
+			
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		}
+	}
+	
+	public static void oneWayAssociationRelationshipMapping(EntityManager em) {
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 		
 		try {
-			Member member = new Member();		
-//			member.setId(100L);
-			member.setName("안녕하세요");
-			member.setMemberType(MemberType.USER);
+			Team teamA = new Team();
+			teamA.setName("teamA");
+			em.persist(teamA);
 			
+			Member member = new Member();	
+			member.setName("안녕하세요");
+//			member.setTeamId(team.getId());
+			member.setTeam(teamA);
 			em.persist(member);
+			
+			// 조회 SQL을 확인하기 위해
+			em.flush();
+			em.clear();
+			
+			// 조회
+//			Member findMember = em.find(Member.class, member.getId());
+//			Long teamId = findMember.getTeamId();
+			
+			// 객체를 테이블에 맞추어 데이터 중심으로 모델링했기 때문에 연관관계가 없음
+//			Team findTeam = em.find(Team.class, teamId);
+			
+			// 조회			
+			Member findMember = em.find(Member.class, member.getId());
+			
+			// 참조를 사용해서 연관관계 조회
+			Team findTeam = findMember.getTeam();
+			findTeam.getName();
+			
+			// 연관관계 수정
+			Team teamB = new Team();
+			teamB.setName("teamB");
+			em.persist(teamB);
+			findMember.setTeam(teamB);
+			
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
-		} finally {
-			em.close();
 		}
+	}
+	
+	public static void bothWayAssociationRelationshipMapping(EntityManager em) {
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
 		
-		emf.close();
+		try {
+			Team teamA = new Team();
+			teamA.setName("teamA");
+			em.persist(teamA);
+			
+			Member member = new Member();
+			member.setName("안녕하세요");
+			member.setTeam(teamA);
+			em.persist(member);
+			
+			// 조회 SQL을 확인하기 위해
+			em.flush();
+			em.clear();
+			
+			// 조회			
+			Member findMember = em.find(Member.class, member.getId());
+			Team findTeam = findMember.getTeam();
+			
+			List<Member> members = findTeam.getMembers();
+			for (Member member1 : members) {				
+				System.out.println("member1 = " + member1);
+			}
+			
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		}
 	}
 }
--- src/main/java/jpa/entity/Member.java
+++ src/main/java/jpa/entity/Member.java
@@ -6,9 +6,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -27,12 +30,13 @@ public class Member {
 	private String name;
 	private int age;
 	
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date regDate;
+//	@Column(name = "TEAM_ID")
+//	private Long teamId;
 	
-	@Enumerated(EnumType.STRING)
-	private MemberType memberType;
-	
+	@ManyToOne(fetch = FetchType.EAGER) // FetchType.LAZY 지연 로딩 
+	@JoinColumn(name = "TEAM_ID")
+	private Team team;
+
 	public Long getId() {
 		return id;
 	}
@@ -45,10 +49,21 @@ public class Member {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public MemberType getMemberType() {
-		return memberType;
+	public int getAge() {
+		return age;
 	}
-	public void setMemberType(MemberType memberType) {
-		this.memberType = memberType;
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Team getTeam() {
+		return team;
+	}
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+	
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", name=" + name + ", age=" + age + ", team=" + team + "]";
 	}
 }
